5ba91278541769c8b856931ea0ec6708cb91be8c
Gayane <gayaneh001@gmail.com>
2019-05-01 16:21:05 +0400

    Initial commit

---
+++ FactorialCountOfZeros/FactorialCountOfZeros/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Numerics;
+
+namespace FactorialCountOfZeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger result = new BigInteger(1);
+            int count = 0;
+            bool input;
+            int num;
+
+            do
+            {
+                Console.Write("Enter number: ");
+                input = int.TryParse(Console.ReadLine(), out num);
+            } while (!input);
+
+            for (int i = 1; i <= num; i++)
+            {
+                result *= i;
+                if (i % 5 == 0)
+                {
+                    count++;
+                    if (i % 25 == 0)
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            Console.WriteLine(result);
+            Console.WriteLine($"Number of zeros in the end is: {count}");
+        }
+    }
+}
