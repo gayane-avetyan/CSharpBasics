3bbb3f431e14269991d84b6167b1f93c35c96d18
Gayane <gayaneh001@gmail.com>
2019-05-30 12:14:10 +0400

    Initial commit

---
+++ FromOneFileToAnother/FromOneFileToAnother/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FromOneFileToAnother
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path1 = @"MyFiles\first.txt";
+            string path2 = @"MyFiles\second.txt";
+            using (FileStream stream = new FileStream(path2, FileMode.Append))
+            {
+                using (StreamReader read = new StreamReader(path1))
+                {
+                    string line = read.ReadLine();
+                    int lineCount = 1;
+                    while (line != null)
+                    {                                               
+                        byte[] array = Encoding.Default.GetBytes($"{lineCount} {line}");
+                        line = read.ReadLine();
+                        stream.Write(array, 0, array.Length);
+                        lineCount++;
+                    }
+                }
+            }
+        }
+    }
+}
