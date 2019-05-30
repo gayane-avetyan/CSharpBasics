fccf753f59a94f9b4de71731088b21a67a96fcc8
Gayane <gayaneh001@gmail.com>
2019-05-30 15:48:01 +0400

    Initial commit

---
+++ SortingInFile/SortingInFile/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingInFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathNames = @"MyFiles\names.txt";
+            string pathSortedNames = @"MyFiles\sortedNames.txt";
+            List<string> names = new List<string>();
+            using (StreamReader stream = new StreamReader(pathNames))
+            {
+                string line = stream.ReadLine();
+                while (line != null)
+                {
+                    names.Add(line);
+                    line = stream.ReadLine();
+                }
+            }
+            names.Sort();
+            using (FileStream stream = new FileStream(pathSortedNames, FileMode.Append))
+            {
+                foreach (var item in names)
+                {
+                    byte[] array = Encoding.Default.GetBytes(item);
+                    stream.Write(array, 0, array.Length);
+                }
+            }
+        }
+    }
+}
---
+++ XmlSerialization/XmlSerialization/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace XmlSerialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Hasmik", 11);
+
+            XmlSerializer serializer = new XmlSerializer(typeof(Person));
+
+            using (FileStream stream = new FileStream("persons.xml", FileMode.OpenOrCreate))
+            {
+                serializer.Serialize(stream, person);
+            }
+        }
+    }
+}
