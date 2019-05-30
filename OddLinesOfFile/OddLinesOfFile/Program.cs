6b568dde5695e6fc22ab76028ab778118917fef3
Gayane <gayaneh001@gmail.com>
2019-05-30 10:28:57 +0400

    Initial commit

---
+++ OddLinesOfFile/OddLinesOfFile/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddLinesOfFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"MyFiles\file.txt";
+            using (StreamReader stream = new StreamReader(path))
+            {
+                string line = stream.ReadLine();
+                int lineCount = 1;           
+                while (line != null)
+                {
+                    if (lineCount % 2 != 0)
+                    {
+                        Console.WriteLine($"{lineCount} {line}");
+                    }
+                    line = stream.ReadLine();
+                    lineCount++;
+                }
+            }
+        }
+    }
+}
