0d3db0de598f905685a041c67f63fc17661aaaf0
Gayane <gayaneh001@gmail.com>
2019-05-31 15:53:26 +0400

    Initial commit

---
+++ SqrtException/SqrtException/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqrtException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Input a number: ");
+                int number = int.Parse(Console.ReadLine());
+                double result = Math.Sqrt(number);
+                if (double.IsNaN(result))
+                {
+                    throw new ArithmeticException();
+                }
+                Console.WriteLine($"The result is {result}");              
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Digits required");
+            }
+            catch (ArithmeticException)
+            {
+                Console.WriteLine("Only positive number is allowed");
+            }
+            finally
+            {
+                Console.WriteLine("Good by");
+            }
+        }
+    }
+}
