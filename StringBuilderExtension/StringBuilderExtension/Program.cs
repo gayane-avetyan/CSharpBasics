5a5ca40cc5ecde4eb400abef97e868b1019be814
Gayane <gayaneh001@gmail.com>
2019-06-02 11:38:03 +0400

    Initial commit

---
+++ StringBuilderExtension/StringBuilderExtension/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringBuilderExtension
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder str = new StringBuilder("123456789");
+            Console.WriteLine(str.Substring(2, 4));
+        }
+    }
+}
---
+++ StringBuilderExtension/StringBuilderExtension/StringBuilderExtension.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringBuilderExtension
+{
+    public static class StringBuilderExtension
+    {
+        public static StringBuilder Substring(this StringBuilder str, int startIndex)
+        {
+            string x = str.ToString();
+            StringBuilder substring = new StringBuilder(x.Substring(startIndex));
+            return substring;
+        }
+
+        public static StringBuilder Substring(this StringBuilder str, int startIndex, int length)
+        {
+            string x = str.ToString();
+            return new StringBuilder(x.Substring(startIndex, length));
+        }
+    }
+}
