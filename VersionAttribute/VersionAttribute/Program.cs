6dafc54fcfeb7df94a86f258ec8abc16557953e5
Gayane <gayaneh001@gmail.com>
2019-06-03 09:47:48 +0400

    Initial commit

---
+++ VersionAttribute/VersionAttribute/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VersionAttribute
+{
+    [Version("2.11")]
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type type = typeof(Program);
+            object[] allAttributes = type.GetCustomAttributes(false);
+            foreach (VersionAttribute item in allAttributes)
+            {
+                Console.WriteLine(item.Version);
+            }
+        }
+    }
+}
---
+++ VersionAttribute/VersionAttribute/VersionAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VersionAttribute
+{
+    [AttributeUsage(AttributeTargets.Struct | AttributeTargets.Class | AttributeTargets.Interface | AttributeTargets.Enum | AttributeTargets.Method)]
+    class VersionAttribute : Attribute
+    {
+        public string Version { get; private set; }
+        public VersionAttribute(string version)
+        {
+            Version = version;
+        }
+    }
+}
